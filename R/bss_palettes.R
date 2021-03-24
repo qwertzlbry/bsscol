503cc6e9cce48abe67442c01acdb6fccbda2f496
qwertzlbry <theesoscar@gmail.com>
2021-03-24 09:42:58 +0100

    new pallettes incoming

--- R/bss_palettes.R
+++ R/bss_palettes.R
@@ -4,11 +4,28 @@
 #'
 bss_palettes <- list(
   `monochrome_black1` = bss_colors %>% filter(col_pal==1) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+  `black_yellow`      = c(bss_colors %>% filter(col_pal==1) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                          bss_colors %>% filter(col_pal==8) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
   `monochrome_green`  = bss_colors %>% filter(col_pal==6) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
   `monochrome_red`    = bss_colors %>% filter(col_pal==7) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+  `red_green`         = c(bss_colors %>% filter(col_pal==7) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                          bss_colors %>% filter(col_pal==6) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
+  `red_yellow`        = c(bss_colors %>% filter(col_pal==7) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                           bss_colors %>% filter(col_pal==8) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
   `monochrome_yellow` = bss_colors %>% filter(col_pal==8) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
   `monochrome_blue`   = bss_colors %>% filter(col_pal==9) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
-  `monochrome_blue_2` = bss_colors %>% filter(col_pal==9) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+  `blue_yellow`       = c(bss_colors %>% filter(col_pal==9) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                          bss_colors %>% filter(col_pal==8) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
+  `blue_green`        = c(bss_colors %>% filter(col_pal==9) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                         bss_colors %>% filter(col_pal==6) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
+  `blue_red`          = c(bss_colors %>% filter(col_pal==9) %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
+                          bss_colors %>% filter(col_pal==7) %>% arrange(-Typ) %>% select(hashed_hex) %>% t()),
+
   `main`              = bss_colors %>% filter(Typ==0)     %>% arrange(Typ) %>% select(hashed_hex) %>% t(),
   `main_only_color`   = bss_colors %>% filter(Typ==0,col_pal==5|
                                              col_pal==6|
